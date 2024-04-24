dc791176c3b9de4043c8725dbbfa55593a391c49
Angel <145053589+Angel170605@users.noreply.github.com>
2024-04-24 14:39:11 +0100

    Update fraction.py

--- UT4/poo/fraction.py
+++ UT4/poo/fraction.py
@@ -8,26 +8,30 @@ class Fraction:
 
     def __add__(self, other: Fraction):
         den = self.den * other.den
-        num = (self.num * (den / self.den)) + (other.num * (den / other.den))
-        return Fraction(num, den)
+        num = (self.num * (den // self.den)) + (other.num * (den // other.den))
+        simply = Fraction.gcd(num, den)
+        return Fraction((num // simply), (den // simply))
 
     def __sub__(self, other: Fraction):
         den = self.den * other.den
-        num = (self.num * (den / self.den)) + (other.num * (den / other.den))
-        return Fraction(num, den)
+        num = (self.num * (den // self.den)) - (other.num * (den // other.den))
+        simply = Fraction.gcd(num, den)
+        return Fraction((num // simply), (den // simply))
 
     def __mul__(self, other: Fraction):
         den = self.den * other.den
         num = self.den * other.den
-        return Fraction(num, den)
+        simply = Fraction.gcd(num, den)
+        return Fraction((num // simply), (den // simply))
 
     def __truediv__(self, other: Fraction):
         den = self.den * other.num
         num = self.num * other.den
-        return Fraction(num, den)
+        simply = Fraction.gcd(num, den)
+        return Fraction((num // simply), (den // simply))
 
-    def __str__(self):
-        return f''
+    def __str__(self) -> str:
+        return f'La solución es {self.num}/{self.den}'
 
     @staticmethod
     def gcd(a: int, b: int) -> int:
