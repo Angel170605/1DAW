c50e0f86b3dc378d3593a5a689062bb11d8818a1
Angel <145053589+Angel170605@users.noreply.github.com>
2024-05-09 16:25:16 +0100

    Update date.py

--- ut4/ejer/poo/date.py
+++ ut4/ejer/poo/date.py
@@ -9,7 +9,9 @@ class Date:
         Ojo con los años bisiestos.
         El 1-1-1900 fue lunes.
         '''
-        ...
+        self.day = day
+        self.month = month
+        self.year = year
 
     @staticmethod
     def is_leap_year(year: int) -> bool:
@@ -28,31 +30,45 @@ class Date:
             case _:
                 return 31
 
-        return True
-
     def get_delta_days(self) -> int:
         '''Número de días transcurridos desde el 1-1-1900 hasta la fecha'''
         ...
-        transcurred = self.day + Date.days_in_month(month)
-        for i in range(1900, year)
-        return 1
+        delta_days = self.day + Date.days_in_month(self.month, self.year)
+        for y in range(1900, self.year):
+            if Date.is_leap_year(y):
+                delta_days += 365
+            else:
+                delta_days += 366
+        return delta_days
 
     @property
     def weekday(self) -> int:
         '''Día de la semana de la fecha (0 para domingo, ..., 6 para sábado).'''
-        ...
+        return int(self.day % 8)
 
     @property
-    def is_weekend(self) -> bool: ...
+    def is_weekend(self) -> bool:
+        return Date.weekday(self) == 0 or Date.weekday(self) == 6
 
     @property
     def short_date(self) -> str:
         '''02/09/2003'''
-        ...
+        if 0 > self.day > 31:
+            self.day = 1
+        elif:
+            0 < self.day > 10:
+                self.day = '0' + str(self.day)
+        if 0 > self.month > 12:
+            self.month = 1
+        if 1900 > self.year > 2050:
+            self.year = 1900
+
+        return f'{self.day}/{self.month}/{self.year}'
 
     def __str__(self):
         '''MARTES 2 DE SEPTIEMBRE DE 2003'''
-        ...
+        WEEK_DAYS = {'DOMINGO':0, 'LUNES':1, 'MARTES':3, 'MIÉRCOLES':4, 'JUEVES':5, 'VIERNES':6, 'SÁBADO':7 }
+        YEAR_MONTHS = {'ENERO':1, 'FEBRERO':2, 'MARZO':3, 'ABRIL':4, 'MAYO':5, 'JUNIO':6, 'JULIO':7, 'AGOSTO':8, 'SEPTIEMBRE':9}
 
     def __add__(self, days: int) -> Date:
         '''Sumar un número de días a la fecha'''
